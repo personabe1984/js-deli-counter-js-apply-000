455764e9cd083a2a5a70fb7d83c2862cb52becdb
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-23 18:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(){
-  
+function currentLine(line){
+  if (line === undefined || line.length === 0){
+    return "The line is currently empty.";
+  }else{
+    for(let i = 0; )
+  }
 }
 
 //var katzDeliLine = [];
