23bf4492518133200ed9a92aebff0084da1c5932
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-23 19:03:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line){
   if (line === undefined || line.length === 0){
     return "The line is currently empty.";
   }else{
-    tempString = "The line is currently:";
+    var tempString = "The line is currently:";
     for(let i = 0; i < line.length; i++){
       tempString += ` ${i+1}. ${line[i]},`;
     }
