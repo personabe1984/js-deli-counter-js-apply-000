6caf26f45323ae0a2b35f71b7f52fa416a2e615d
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-23 18:37:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, name){
-  //katzDeliLine.push(name);
+  katzDeliLine.push(`Welcome, ${name}. You are number ${katzDeliLine.length-1} in line.`);
   return katzDeliLine.length-1;
 }
 
